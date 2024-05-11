f37ae60e73869bb344442acad359ccc21e111500
gustavogalazzo <gustavogalazzo2000@gmail.com>
2024-05-10 21:22:14 -0300

    segunda atualização do site

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-;(function () {
-  let socials = document.querySelectorAll('.social div')
-
-  socials.forEach(function (social, index) {
-    social.style.animation = `moveIn 1s ease-in-out forwards ${
-      index / 7 + 0.2
-    }s`
-  })
-})
